10576d0ecfff827ea5c7b89caa9fb2468ed004de
LuisEnMarroquin <mluis651@gmail.com>
2019-10-04 09:02:12 -0500

    feat: Added new file

---
+++ Trash.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Trash {
+  public static void main(String[] args) {
+    int m = 6;
+    System.out.println(m*=3);
+    int a = 10;
+    do {
+      System.out.println(a);
+      a-=2;
+    } while(a>=0);
+    for(int z=10; z>=1; z--){
+      System.out.println("z" + z);
+    }
+  }
+}
